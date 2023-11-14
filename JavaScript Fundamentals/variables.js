4cbf197c881be48c962bceecf9a2580d9b4b0947
Giotu <evilalex1998@mail.ru>
2023-11-14 22:02:15 +0300

    feat:add tasks topic variables

---
+++ JavaScript Fundamentals/variables.js
@@ -0,0 +1,16 @@
+// Task 1
+// Объявите две переменные: admin и name.
+// Запишите строку "Джон" в переменную name.
+// Скопируйте значение из переменной name в admin.
+// Выведите на экран значение admin, используя функцию alert (должна показать «Джон»).
+
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin); // заменил alert
+
+//Task 2
+
+let ourPlanetName = "Earth";
+let currentUserName = "Valera";
