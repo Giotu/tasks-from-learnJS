a2bd71a013269f98c09de9012353501e43689b0a
Giotu <evilalex1998@mail.ru>
2023-12-05 14:58:06 +0300

    feat: add tasks from topic string

--- data-types/string.js
+++ data-types/string.js
@@ -5,3 +5,44 @@ console.log(ucFirst("вася")); // Вася
 function ucFirst(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+// Напишите функцию checkSpam(str), возвращающую true, если str содержит 'viagra' или 'XXX', а иначе false.
+// Функция должна быть нечувствительна к регистру:
+
+console.log(checkSpam("buy ViAgRA now"));
+console.log(checkSpam("free xxxxx"));
+console.log(checkSpam("innocent rabbit"));
+
+function checkSpam(str) {
+	let newStr = str.toLowerCase();
+	return newStr.includes("xxx") || newStr.includes("viagra");
+}
+
+// Создайте функцию truncate(str, maxlength), которая проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на "…", так, чтобы её длина стала равна maxlength.
+// Результатом функции должна быть та же строка, если усечение не требуется, либо, если необходимо, усечённая строка.
+// Например:
+
+// truncate("Вот, что мне хотелось бы сказать на эту тему:", 20) = "Вот, что мне хотело…"
+
+// truncate("Всем привет!", 20) = "Всем привет!"
+
+const truncate = (str, maxlength) => {
+	if (str.length > maxlength) {
+		str = str.slice(0, maxlength - 1) + "…";
+	}
+	return str;
+};
+
+console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 20));
+
+console.log(truncate("Всем привет!", 20));
+
+// Есть стоимость в виде строки "$120". То есть сначала идёт знак валюты, а затем – число.
+// Создайте функцию extractCurrencyValue(str), которая будет из такой строки выделять числовое значение и возвращать его.
+// Например:
+
+console.log(extractCurrencyValue("$120") === 120); // true
+
+function extractCurrencyValue(str) {
+	return Number(str.slice(1));
+}
