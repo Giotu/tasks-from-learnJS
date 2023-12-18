731de855be745e1f94ae6ffdee445869c58b24eb
Giotu <evilalex1998@mail.ru>
2023-12-18 13:18:29 +0300

    feat: add tasks from topic map-set

---
+++ data-types/map-set.js
@@ -0,0 +1,39 @@
+// Допустим, у нас есть массив arr.
+// Создайте функцию unique(arr), которая вернёт массив уникальных, не повторяющихся значений массива arr.
+// Например:
+
+function unique(arr) {
+	return Array.from(new Set(arr));
+}
+
+let values = [
+	"Hare",
+	"Krishna",
+	"Hare",
+	"Krishna",
+	"Krishna",
+	"Krishna",
+	"Hare",
+	"Hare",
+	":-O",
+];
+
+console.log(unique(values)); // Hare,Krishna,:-O
+
+// Анаграммы – это слова, у которых те же буквы в том же количестве, но они располагаются в другом порядке.
+// Например:
+// nap - pan
+// ear - are - era
+// cheaters - hectares - teachers
+// Напишите функцию aclean(arr), которая возвращает массив слов, очищенный от анаграмм.
+// Например:
+
+function aclean(arr) {
+	let map = new Map();
+	arr.map((item) => map.set(item.toLowerCase().split("").sort().join(""), item));
+	return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log(aclean(arr));
